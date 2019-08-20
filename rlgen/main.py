61f9f49b3f3dfad7029cb0e4411eb8baacf864c8
Guillaume Gonnet <Guillaume.Gonnet@grenoble-inp.org>
2019-08-20 23:26:49 +0200

    Generator bae files.

---
+++ rlgen/main.py
@@ -0,0 +1,16 @@
+"""
+
+Entry point of this utility.
+
+Copyright (C) 2019, Team RedLogic.
+All rights reserved.
+
+"""
+
+from .mceditlib.worldeditor import WorldEditor
+
+
+def main():
+    "Entry point of this utility."
+
+    # TODO.
