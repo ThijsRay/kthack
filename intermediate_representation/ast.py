1ba2ad634542ed8c6074e6333863b4c53a62f1e7
Thijs Raymakers <thijs@raymakers.nl>
2019-08-21 02:22:22 +0200

    Add depth function

--- intermediate_representation/ast.py
+++ intermediate_representation/ast.py
@@ -4,51 +4,92 @@
 #   Not(x)
 #   Terminal variables
 
+from
 
 class Operation:
     """
     Class that defines the AST types
     """
     def simplify(self):
-        raise NotImplementedError("Class %s doesn't implement simplyfy()" % (self.__class__.__name__))
+        raise NotImplementedError("Class %s doesn't implement simplify()" % (self.__class__.__name__))
+
+    def depth(self):
+        raise NotImplementedError("Class %s doesn't implement depth()" % (self.__class__.__name__))
+
+    def module(self):
+        raise NotImplementedError("Class %s doesn't implement module()" % (self.__class__.__name__))
 
 
 class And(Operation):
     def __init__(self, x, y):
         self.x = x  # Operation
         self.y = y  # Operation
+        self.depth = 0
 
     def simplify(self):
-        And(self.x.simplify(), self.y.simplify())
+        return And(self.x.simplify(), self.y.simplify())
 
+    def depth(self, parent_depth = 0):
+        if self.depth == 0:
+            self.depth = parent_depth + 1
+            self.x.depth(self.depth)
+            self.y.depth(self.depth)
 
 class Or(Operation):
     def __init__(self, x, y):
         self.x = x  # Operation
         self.y = y  # Operation
+        self.depth = 0
 
     def simplify(self):
-        Or(self.x.simplify(), self.y.simplify())
+        return Or(self.x.simplify(), self.y.simplify())
+
+    def depth(self, parent_depth = 0):
+        if self.depth == 0:
+            self.depth = parent_depth + 1
+            self.x.depth(self.depth)
+            self.y.depth(self.depth)
 
 
 class Not(Operation):
     def __init__(self, x):
         self.x = x  # Operation
+        self.depth = 0
 
     def simplify(self):
-        Not(self.x.simplify())
+        return Not(self.x.simplify())
+
+    def depth(self, parent_depth = 0):
+        if self.depth == 0:
+            self.depth = parent_depth + 1
+            self.x.depth(self.depth)
 
 
 class Terminal(Operation):
     def __init__(self, identifier):
         self.identifier = identifier  # String
+        self.depth = 0
 
     def simplify(self):
-        Terminal(self.identifier)
+        return self
 
+    def depth(self, parent_depth = 0):
+        if self.depth == 0:
+            self.depth = parent_depth + 1
+
+
+class Pass(Operation):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def simplify(self):
+        return Pass(self.left.simplify(), self.right.simplify())
+
+    def depth(self, parent_depth = 0):
+        parent_depth
 
 if __name__ == "__main__":
-
     input = And(Or(Or("x", And("y", "z")), "x"), Or("x", Not(And("a", "x"))))
     input.simplify()
 
